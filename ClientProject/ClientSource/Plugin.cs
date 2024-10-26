f09c98cf7613710163d550d734180d08a73c66cb
zhurengong <2731412072@qq.com>
2024-10-26 12:28:52 +0800

    init solution

---
+++ ClientProject/ClientSource/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Barotrauma;
+using Barotrauma.Items.Components;
+using Barotrauma.Networking;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using MoonSharp.Interpreter;
+
+namespace BetterFabricatorUI
+{
+    public partial class Plugin : IAssemblyPlugin
+    {
+        [HarmonyPatch(declaringType: typeof(Fabricator))]
+        [HarmonyPatch(methodName: nameof(Fabricator.CreateGUI))]
+        class Patch_Fabricator_CreateGUI
+        {
+            [HarmonyPostfix]
+            static void Inject(Fabricator __instance)
+            {
+                var fabricator = __instance;
+
+            }
+        }
+    }
+}
